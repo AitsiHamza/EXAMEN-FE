a873faf4a13219d60491ca47bee9c323f5e999ca
AitsiHamza <hamzaaitsi65@gmail.com>
2022-01-21 12:31:57 +0100

    commit

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+import { onMount } from 'svelte';
+
+	let movies = [];
+
+	onMount(async function() {
+		fetch("http://localhost:8000")
+		.then((res) => res.json())
+		.then((res) => {
+			movies = res;
+		});
+	});
+</script>
+
+<main>
+<table>
+	<thead>
+		<tr>
+			<th>Titre</th>
+			<th>Genres</th>
+			<th>Year</th>
+		</tr>
+	</thead>
+		<tbody>
+			{#each movies as movie}
+				<tr>
+					<td>{movie.title}</td>
+					<td>{movie.genres.join(', ')}</td>
+					<td>{movie.year}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
